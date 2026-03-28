3a5e2bd76b02f33a8af5978e58a465fd73d61b07
Hyjaxaru <noah.albrock@gmail.com>
2026-03-28 14:59:30 +0000

    Vector math moved to it's own namespace
    I thought that the vector math functions I was using for the PlayerGun class would be useful in other places.
    
    These functions also didn't make sense in a dedicated class, but I also wanted some seperation, so I put them in a namespace

--- Coursework/CMP105App/PlayerGun.cpp
+++ Coursework/CMP105App/PlayerGun.cpp
@@ -17,10 +17,10 @@ PlayerGun::~PlayerGun()
 void PlayerGun::setPositionFromReference(sf::Vector2f origin, sf::Vector2f target)
 {
 	// calculate the angle from the reference position to the target position
-	auto angle = calculatePointAngle(origin, target).asRadians();
+	auto angle = vm::calculatePointAngle(origin, target).asRadians();
 
 	// calculate the position the gun should be placed at
-	auto dist = calculatePointDistance(origin, target);
+	auto dist = vm::calculatePointDistance(origin, target);
 	auto distC = std::clamp(dist, 0.f, MAX_DIST_FROM_REFERENCE);
 	auto pos = sf::Vector2f{
 		origin.x + distC * std::sin(angle),
@@ -39,31 +39,11 @@ void PlayerGun::setPositionFromReference(sf::Vector2f origin, sf::Vector2f targe
 	setScale(sf::Vector2f{ isFacingBack ? -1.f : 1.f, 1.f });
 }
 
-sf::Angle PlayerGun::calculatePointAngle(sf::Vector2f origin, sf::Vector2f target)
-{
-	auto diff = target - origin;
-	return sf::radians(std::atan2(diff.x, diff.y));
-}
-
-float PlayerGun::calculatePointDistance(sf::Vector2f origin, sf::Vector2f target)
-{
-	auto diff = target - origin;
-	return std::sqrt(diff.x * diff.x + diff.y * diff.y);
-}
-
-sf::Vector2f PlayerGun::calculateVector(sf::Vector2f origin, sf::Angle angle, float length) {
-	auto radians = angle.asRadians();
-	return sf::Vector2f{
-		origin.x + length * std::sin(radians),
-		origin.y + length * std::cos(radians)
-	};
-}
-
 sf::Vector2f PlayerGun::fireGunWithRecoil() {
 	// TODO: Fire the gun
 
 	// claculate and return the recoil force
 	auto rotation = getRotation().asRadians();
 	auto angle = rotation - PI_ESTIMATION;
-	return calculateVector(sf::Vector2f(), sf::radians(angle), RECOIL_FORCE);
+	return vm::calculateVector(sf::Vector2f(), sf::radians(angle), RECOIL_FORCE);
 }
--- Coursework/CMP105App/PlayerGun.h
+++ Coursework/CMP105App/PlayerGun.h
@@ -5,6 +5,7 @@
 #include "Framework/GameObject.h"
 
 #include "AssetManager.h"
+#include "VectorMath.h"
 
 class PlayerGun :
     public GameObject
@@ -18,14 +19,6 @@ public:
     void setPositionFromReference(sf::Vector2f origin, sf::Vector2f target);
 
 private:
-
-    // calcuate the position and rotation of the gun
-    inline sf::Angle calculatePointAngle(sf::Vector2f origin, sf::Vector2f target);
-    inline float calculatePointDistance(sf::Vector2f origin, sf::Vector2f target);
-    inline sf::Vector2f calculateVector(sf::Vector2f origin, sf::Angle angle, float length);
-
-    // --- Constants --- //
-
     const float PI_ESTIMATION = 3.141592654f;
     const float MAX_DIST_FROM_REFERENCE = 64.f;
     const float RECOIL_FORCE = 50.f;
