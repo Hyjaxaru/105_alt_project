c665a0f18a252608bbfdc39a92df35d6ce719459
Hyjaxaru <noah.albrock@gmail.com>
2026-04-28 16:04:00 +0100

    tileset loading logic boilerplate logic
    when loading the tileset, we check if the level defines using the default option. if not, we use the path provided.
    
    if the level defines default and no default is set we fail. if default is not requested, we try and load the path the level provides. if that fails too, then we fail.

--- Coursework/CMP105App/LevelManager.cpp
+++ Coursework/CMP105App/LevelManager.cpp
@@ -17,7 +17,7 @@ void LevelManager::loadLevel(std::string filePath)
 
 	// decode all of the keywords 
 	std::string keyword, levelName;
-	bool validLevel, foundVersion = false;
+	bool validLevel = false, validVersion = false, validTileset = false;
 
 	while (file >> keyword)
 	{
@@ -35,15 +35,57 @@ void LevelManager::loadLevel(std::string filePath)
 
 		// detect the minimum version of the loader required to load the level
 		// helps with versioning if this continues
-		if (keyword == "VERSION" && !foundVersion)
+		else if (keyword == "VERSION" && !validVersion)
 		{
 			int version;
 			file >> version;
 
+			// fail if the file is too new
 			if (version > LOADER_VERSION) {
 				LOG_ERROR(makeFileLoadFailErrorMessage(filePath, "Newer loader version required."));
 				return;
 			}
+
+			// the version number is valid!
+			validVersion = true;
+		}
+
+		// get the name of the tileset to use
+		else if (keyword == "TILESET" && !validTileset)
+		{
+			std::string tilesetPath;
+			file >> tilesetPath;
+			bool useDefault = tilesetPath == "default";
+
+			// check if the tileset is loaded into AssetManager. if not we fail
+			auto& assets = AssetManager::Instance();
+			bool loaded = false;
+			if (useDefault)
+				loaded = assets.getTexture(AssetManager::Defaults::TILEMAP) != nullptr;
+			else
+				loaded = assets.getTexture(tilesetPath) != nullptr;
+
+			// if we are using default and we cant find it, fail load immediately
+			if (!loaded && useDefault)
+			{
+				LOG_ERROR(makeFileLoadFailErrorMessage(filePath, "Attempted to use default tileset before load."));
+				return;
+			}
+
+			// if we arnt using default, and the tileset isnt loaded, try load it.
+			// if we cant, fail the level load as the tiles don't exist at all
+			else if (!loaded && !useDefault)
+			{
+				auto* result = assets.loadTexture(tilesetPath, levelName);
+				if (result == nullptr)
+				{
+					LOG_ERROR(makeFileLoadFailErrorMessage(filePath, "Tileset '" + tilesetPath + "' does not exist"));
+					return;
+				}
+			}
+			
+			// finish up the stage
+			validTileset = true;
 		}
 	}
 }
